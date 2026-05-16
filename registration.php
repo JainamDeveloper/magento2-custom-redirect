43512e5dad84ab235f58156aef7f82cac46c545f
jainam shah <43310942+JainamDeveloper@users.noreply.github.com>
2026-05-16 21:24:27 +0530

    chore: prepare composer.json for Packagist submission

--- registration.php
+++ registration.php
@@ -1,6 +1,3 @@
 <?php
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Magneto_CustomRedirect',
-    __DIR__
-);
+use Magento\Framework\Component\ComponentRegistrar;
+ComponentRegistrar::register(ComponentRegistrar::MODULE,'Magneto_CustomRedirect',__DIR__);
